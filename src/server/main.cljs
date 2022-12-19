d4dced0f2be6f3e2a8d3bd023badf3297cd27696
Paul Cristian <paul931224@gmail.com>
2022-12-19 06:57:10 +0100

    A little express example for nodejs.

--- src/server/main.cljs
+++ src/server/main.cljs
@@ -1,13 +1,34 @@
 
-(ns server.main)
+(ns server.main
+  (:require ["express" :as express]))
 
 (def value-a 1)
 
 (defonce value-b 2)
 
-(defn ^:dev/after-load reload! []
+
+
+(set! *warn-on-infer* true)
+
+(defonce server (atom nil))
+
+(defn start-server []
+  (println "Starting server")
+  (let [app (express)]
+    (.get app "/" (fn [req res] (.send res "Hello, world")))
+    (.listen app 3000 (fn [] (println "Example app listening on port 3000!")))))
+
+(defn ^:dev/before-load stop! []
   (println "Code updated.")
-  (println "Trying values:" value-a value-b))
+  (println "Trying values:" value-a value-b)
+  (.close @server)
+  (reset! server nil))
+
+(defn ^:dev/after-load start! []
+  (println "Code updated.")
+  (println "Trying values:" value-a value-b)
+  (reset! server (start-server)))
 
 (defn main! []
-  (println "App loaded!"))
+  (println "Adsapp loaded!")
+  (start!))
