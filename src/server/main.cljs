336fbcad5117f3faeebca72352322525275bdd08
Paul Cristian <paul931224@gmail.com>
2022-12-19 07:23:04 +0100

    Added express. Hot reload works well when the value of the route is behind a variable or function.

--- src/server/main.cljs
+++ src/server/main.cljs
@@ -2,33 +2,43 @@
 (ns server.main
   (:require ["express" :as express]))
 
-(def value-a 1)
-
-(defonce value-b 2)
-
-
-
-(set! *warn-on-infer* true)
 
 (defonce server (atom nil))
+
+(def index-text "Index")
+
+(def hello-text "Hello there D3V!")
+
+(def products [{:name "Neuralink"
+                :price 10000}
+               {:name "Tesla"
+                :price 30000}])
+
+(defn map-products []
+  (str
+   "Products: "
+   (mapv :name products) 
+   " - Total-price: "
+   (reduce + (map :price products))))
 
 (defn start-server []
   (println "Starting server")
   (let [app (express)]
-    (.get app "/" (fn [req res] (.send res "Hello, world")))
-    (.listen app 3000 (fn [] (println "Example app listening on port 3000!")))))
+    (.get app "/"       (fn [req res]  (.send res index-text)))
+    (.get app "/hello"  (fn [req res]  (.send res  hello-text)))
+    (.get app "/mapv"   (fn [req res]  (.send res (map-products))))
+    (.listen app 3000   (fn [] (println "Example app listening on port 3000!")))))
+ 
 
 (defn ^:dev/before-load stop! []
-  (println "Code updated.")
-  (println "Trying values:" value-a value-b)
-  (.close @server)
-  (reset! server nil))
+  (println "Code updated before.")
+  (when @server (.close @server)))
+  
 
 (defn ^:dev/after-load start! []
-  (println "Code updated.")
-  (println "Trying values:" value-a value-b)
+  (println "Code updated after.") ;index-text) 
   (reset! server (start-server)))
 
 (defn main! []
-  (println "Adsapp loaded!")
+  (println "App loaded!")
   (start!))
